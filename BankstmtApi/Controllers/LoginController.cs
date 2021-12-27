11f58d8da59765b250d36a2dcc89fcc770c13279
unknown <Administrator@KANINI-LTP-459.gisbizdev.gisbiz.chennai.com>
2021-12-27 19:20:09 +0530

    final api

---
+++ BankstmtApi/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using BankstmtApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankstmtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private IMongoCollection<UserLogin> users;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("getAllUsers")]
+        public JsonResult getAllUsers()
+        {
+            MongoClient mongoClient = new MongoClient(_configuration.GetConnectionString("con1"));
+
+            var dbList = mongoClient.GetDatabase("bankstatement")
+                .GetCollection<UserLogin>("userlogin")
+                .AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+
+        [HttpGet]
+        [Route("LoginBankUser")]
+        public IActionResult LoginBankUser(string username, string password)
+        {
+            MongoClient mongoClient = new MongoClient(_configuration.GetConnectionString("con1"));
+
+            users = mongoClient.GetDatabase("bankstatement")
+                .GetCollection<UserLogin>("userlogin");
+
+            List<UserLogin> userList = users.Find(user => true).ToList();
+
+            foreach(var u in userList)
+            {
+                if(u.username.Equals(username) && u.password.Equals(password))
+                {
+                    return Ok(u.username);
+                }
+            }
+
+            string resStr = "nota";
+            return Ok(resStr);
+        }
+
+    }
+}
---
+++ BankstmtApi/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankstmtApi.Models
+{
+    public class UserLogin
+    {
+        public ObjectId Id { get; set; }
+
+        public string username { get; set; }
+
+        public string password{ get; set; }
+    }
+}
